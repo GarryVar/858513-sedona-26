a4250ab17e8c54f07cb258f72a0a3fc3a6ff2be6
GarryVar <darisvet.sokolov@yandex.ru>
2019-10-03 18:19:09 +0300

    pixel-perfect

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 
-var searchForm = function(){
+var searchForm = function () {
 
   var searchHotel = document.querySelector('.hotel-search--hide');
   var searchBtn = document.querySelector('.button--search');
 
-  searchBtn.addEventListener('click', function(){
+  searchBtn.addEventListener('click', function () {
     searchHotel.classList.toggle('hotel-search--hide');
-    searchHotel.classList.toggle('hotel-search'); 
+    searchHotel.classList.toggle('hotel-search');
   });
 
 };
 
 searchForm();
+
+var personNumber = function () {
+
+  var form = document.querySelector('.hotel-search'),
+    personInput = document.querySelectorAll('.hotel-search__input'),
+    adultPlus = document.querySelector('.hotel-search__adult-value--plus'),
+    adultMinus = document.querySelector('.hotel-search__adult-value--minus');
+
+}
+
+personNumber();
