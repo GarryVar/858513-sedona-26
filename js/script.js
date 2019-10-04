d8cfc3f02b7f08968b8b9c16e14ea49a0745ad9c
GarryVar <darisvet.sokolov@yandex.ru>
2019-10-04 18:37:12 +0300

    Подготовка к защите

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
-var searchHotel = document.querySelector('.hotel-search--hide');
+var searchHotel = document.querySelector('.hotel-search');
 var searchBtn = document.querySelector('.button--search');
+
+var form = document.querySelector('form');
+var inputEntry = searchHotel.querySelector('[name=in]');
+var inputOut = searchHotel.querySelector('[name=in]');
 
 searchBtn.addEventListener('click', function (event) {
   event.preventDefault();
-  searchHotel.classList.toggle('hotel-search--hide');
+
+  searchHotel.classList.toggle('hotel-search--show');
   searchHotel.classList.toggle('hotel-search');
+  inputEntry.focus();
 });
 
 
-// searchBtn.addEventListener('keydown', function (event) {
-//   if (event.keydown === 27) {
-//     if ()
-//       searchHotel.classList.add('hotel-search--hide');
-//     searchHotel.classList.remove('hotel-search');
-//   }
-// });
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (!inputEntry.value || !inputOut.value) {
+    console.log("ЗАПОЛНИ ПОЛЯ!!!")
+  }
+
+  console.log(inputEntry.value, inputOut.value);
+});
+
+
+
 
 
