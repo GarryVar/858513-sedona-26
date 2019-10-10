bfa1bd87200067d1e083fdc7f8e2513e625eca75
GarryVar <darisvet.sokolov@yandex.ru>
2019-10-10 15:22:24 +0300

    Исправление после первой подачкик защите

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 
-var searchHotel = document.querySelector('.hotel-search');
-var searchBtn = document.querySelector('.button--search');
+var searchHotel = document.querySelector(".hotel-search");
+var searchBtn = document.querySelector(".button--search");
 
 
-var inputEntry = searchHotel.querySelector('[name=entry]');
-var inputOut = searchHotel.querySelector('[name=leave]');
+var inputEntry = searchHotel.querySelector("[name=entry]");
+var inputOut = searchHotel.querySelector("[name=leave]");
 
-var inputAdult = searchHotel.querySelector('[name=adult]');
-var inputChild = searchHotel.querySelector('[name=child]');
+var inputAdult = searchHotel.querySelector("[name=adult]");
+var inputChild = searchHotel.querySelector("[name=child]");
 
-var btnPlusAdult = searchHotel.querySelector('.hotel-search__adult-value--plus');
-var btnMinusAdult = searchHotel.querySelector('.hotel-search__adult-value--minus');
-var btnPlusChild = searchHotel.querySelector('.hotel-search__child-value--plus');
-var btnMinusChild = searchHotel.querySelector('.hotel-search__child-value--minus');
+var btnPlusAdult = searchHotel.querySelector(".hotel-search__adult-value--plus");
+var btnMinusAdult = searchHotel.querySelector(".hotel-search__adult-value--minus");
+var btnPlusChild = searchHotel.querySelector(".hotel-search__child-value--plus");
+var btnMinusChild = searchHotel.querySelector(".hotel-search__child-value--minus");
 
 var isStorageSupport = true;
-var storage = '';
+var storage = "";
 
-var summAdult = '';
-var summChild = '';
+var summAdult = "";
+var summChild = "";
 
 try {
 
-  storage = isStorageSupport.getItem('adult', 'child');
+  storage = isStorageSupport.getItem("adult", "child");
 }
 
 catch (err) {
@@ -31,15 +31,15 @@ catch (err) {
 }
 
 
-searchBtn.addEventListener('click', function (event) {
+searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
-if(searchHotel.classList.contains('hotel-search--hide')) {
+if(searchHotel.classList.contains("hotel-search--hide")) {
 
-  searchHotel.classList.remove('hotel-search--hide');
+  searchHotel.classList.remove("hotel-search--hide");
 }
-  searchHotel.classList.add('hotel-search--show');
-  searchHotel.classList.remove('hotel-search');
+  searchHotel.classList.add("hotel-search--show");
+  searchHotel.classList.remove("hotel-search");
 
   if (storage) {
     inputEntry.value = storage;
@@ -48,41 +48,41 @@ if(searchHotel.classList.contains('hotel-search--hide')) {
 });
 
 
-searchHotel.addEventListener('submit', function (event) {
+searchHotel.addEventListener("submit", function (event) {
   event.preventDefault();
 
   if (!inputEntry.value || !inputOut.value) {
-    console.log('Форма не отправилась! Заполните кол-во Взрослых и Детей');
+    console.log("Форма не отправилась! Заполните кол-во Взрослых и Детей");
   }
 
   else {
 
-      console.log('Дата заезда [' + inputEntry.value + ']');
-      console.log('Дата выезда [' + inputOut.value + ']');
-      console.log('Взрослые [' + inputAdult.value + ']');
-      console.log('Дети [' + inputChild.value + ']');
+      console.log("Дата заезда [" + inputEntry.value + "]");
+      console.log("Дата выезда [" + inputOut.value + "]");
+      console.log("Взрослые [" + inputAdult.value + "]");
+      console.log("Дети [" + inputChild.value + "]");
   }
 
 
 });
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener("keydown", function (event) {
 
   if (event.keyCode === 27) {
 
-    if (searchHotel.classList.contains('hotel-search--show')) {
+    if (searchHotel.classList.contains("hotel-search--show")) {
 
       event.preventDefault();
 
-      searchHotel.classList.remove('hotel-search--show');
-      searchHotel.classList.add('hotel-search--hide');
-      searchHotel.classList.add('hotel-search');
+      searchHotel.classList.remove("hotel-search--show");
+      searchHotel.classList.add("hotel-search--hide");
+      searchHotel.classList.add("hotel-search");
       
     }
   }
 });
 
-btnPlusAdult.addEventListener('click', function (event) {
+btnPlusAdult.addEventListener("click", function (event) {
   event.preventDefault();
 
   if(inputAdult.value >= 0) {
@@ -95,7 +95,7 @@ btnPlusAdult.addEventListener('click', function (event) {
 });
 
 
-btnMinusAdult.addEventListener('click', function (event) {
+btnMinusAdult.addEventListener("click", function (event) {
   event.preventDefault();
 
   if(inputAdult.value != 0) {
@@ -108,7 +108,7 @@ btnMinusAdult.addEventListener('click', function (event) {
 });
 
 
-btnPlusChild.addEventListener('click', function(event) {
+btnPlusChild.addEventListener("click", function(event) {
   event.preventDefault();
 
   if(inputChild.value >=0) {
@@ -121,7 +121,7 @@ btnPlusChild.addEventListener('click', function(event) {
 });
 
 
-btnMinusChild.addEventListener('click', function(event) {
+btnMinusChild.addEventListener("click", function(event) {
   event.preventDefault();
 
   if(inputChild.value !=0) {
