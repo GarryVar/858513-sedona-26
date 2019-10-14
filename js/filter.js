9025d16f5c88cea35f743312bf9425322a1718eb
GarryVar <darisvet.sokolov@yandex.ru>
2019-10-14 08:38:11 +0300

    Исправление ошибок после первой защиты

--- js/filter.js
+++ js/filter.js
@@ -1,27 +1,23 @@
+var filterForm = document.querySelector(".filter__form");  
+var filterSubmit = filterForm.querySelector(".filter__submit");
 
-var filterForm = document.querySelector('.filter__form');  
-var filterSubmit = filterForm.querySelector('.filter__submit');
+var filterPriceMin = filterForm.querySelector("[name=min-price]");
+var filterPriceMax = filterForm.querySelector("[name=max-price]");
 
-var filterPriceMin = filterForm.querySelector('[name=min-price]');
-var filterPriceMax = filterForm.querySelector('[name=max-price]');
-
-var filterCheckbox = filterForm.querySelectorAll('[type=checkbox]');
+var filterCheckbox = filterForm.querySelectorAll("[type=checkbox]");
 
 
-	filterForm.addEventListener('submit', function(event) {
-
-		event.preventDefault(); 
+	filterForm.addEventListener("submit", function(event) {
 
 			if (!filterPriceMin.value || !filterPriceMax.value) {
 
-					alert('Форма не отправилась!!!');
-
+				event.preventDefault();
 				
 			} 
 			else {
 
-				console.log('Стоимость от ' + filterPriceMin.value + ' рублей.');
-				console.log('Стоимость до ' + filterPriceMax.value + ' рублей.');
+				console.log("Стоимость от " + filterPriceMin.value + " рублей.");
+				console.log("Стоимость до " + filterPriceMax.value + " рублей.");
 
 				for(var i = 0; i < filterCheckbox.length; i++) {
 					console.log(filterCheckbox[i].checked  && filterCheckbox[i].name);
